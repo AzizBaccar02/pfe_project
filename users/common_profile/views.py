97d664ef7becffca28de0e7c9a6185e35eb7ad35
AzizBaccar02 <azizbaccar79@gmail.com>
2026-05-05 12:07:19 +0100

    Save backend changes before pulling main

--- users/common_profile/views.py
+++ users/common_profile/views.py
@@ -4,13 +4,25 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
 
-from .serializers import MeSerializer, ProfileUpdateSerializer
+from .serializers import MeSerializer, MeUpdateSerializer, ProfileUpdateSerializer
+
 
 class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return Response(MeSerializer(request.user).data)
+
+    def patch(self, request):
+        serializer = MeUpdateSerializer(
+            request.user,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(MeSerializer(request.user).data, status=status.HTTP_200_OK)
 
 
 class MyProfileView(APIView):
