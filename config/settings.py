c9acebdd83e3946d9279de170c05625ae7da857f
AzizBaccar02 <azizbaccar79@gmail.com>
2026-05-11 10:59:11 +0100

    11/05/26 agent integration

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jobmatch_db',
         'USER': 'postgres',
-        'PASSWORD': '1966',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
